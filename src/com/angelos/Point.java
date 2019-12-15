b5906c2b37710a6a052eda200838c3685343f7c1
Angelos <toutziaris.angelos@outlook.com>
2019-12-15 18:57:32 +0200

    Add Point class for a definition of a point in a 2D Matrix

---
+++ src/com/angelos/Point.java
@@ -0,0 +1,22 @@
+package com.angelos;
+
+/**
+ * Definition of a point's coordinates in a 2D Matrix.
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
