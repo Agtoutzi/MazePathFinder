0ebc37415fa90ba3979433fb1e97d6f79a2f12d0
Angelos <toutziaris.angelos@outlook.com>
2019-12-15 18:51:16 +0200

    Add enum for Point State (START, GOAL, PATH, WALL)

---
+++ src/com/angelos/EPointState.java
@@ -0,0 +1,30 @@
+package com.angelos;
+
+/**
+ * Enum describing the state of a point in the matrix.
+ */
+public enum EPointState {
+    START('S'),
+    GOAL('G'),
+    PATH('_'),
+    WALL('X');
+
+    private char symbol;
+
+    EPointState(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public static EPointState getPointState(char symbol) {
+        for (EPointState EPointState : EPointState.values()) {
+            if (EPointState.getSymbol() == symbol) {
+                return EPointState;
+            }
+        }
+        return null;
+    }
+}
