579fd945d4d20f3081df99c0fdf43f0ed1f2a13d
Angelos <toutziaris.angelos@outlook.com>
2019-12-18 21:02:00 +0200

    Read input-file from command-line arguments

--- src/com/angelos/Main.java
+++ src/com/angelos/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            Maze maze = InputRead.readMazeFromFile("resources/maze2.txt");
-            PathFinder pathFinder = new PathFinder(maze);
+            Maze maze = InputRead.readMazeFromFile(args[0]);
+            IPathFinder pathFinder = new PathFinder(maze);
             Path path = pathFinder.findPath();
             System.out.println(path.toString());
         } catch (Exception e) {
