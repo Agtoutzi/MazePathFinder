67355ea18c96c1d7e5f94dab79c529098e77bb6c
Angelos <toutziaris.angelos@outlook.com>
2019-12-20 09:35:13 +0200

    Add validation for existence of Start/Goal point in read maze + comments-javadoc in InputRead

--- src/main/java/InputRead.java
+++ src/main/java/InputRead.java
@@ -6,35 +6,65 @@ import java.util.List;
 
 public class InputRead {
 
+    /**
+     * Reads a text file with a maze definition and creates a {@link Maze} object.
+     * Validates the maze read from the file.
+     *
+     * @param inputFilePath the path of the text file containing the maze definition
+     * @return the created maze
+     * @throws InvalidInputException when the definition of the maze is invalid. The maze must have a number of rows and
+     *                               columns limited between 2 and {@link Integer#MAX_VALUE}, all rows(lines of text)
+     *                               must have the same length, and the text must contain only '_', 'X' and exactly 1
+     *                               'S' and 'G' character
+     * @throws IOException           when a problem occurs when reading the file
+     */
     public static Maze readMazeFromFile(String inputFilePath) throws InvalidInputException, IOException {
         List<EPointState[]> statesList = new ArrayList<>();
         Maze maze = new Maze();
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilePath))) {
 
+            // Read first line of file and validate the size of it
             String line = reader.readLine();
             int firstLineLength = line.length();
             validateFileSize(firstLineLength);
 
             int lineIndex = 0;
             while (line != null) {
+                // Validate the last read line
                 validateLineLength(line, firstLineLength);
+
+                // Add a new array of point-states from the last read line
                 statesList.add(createPointStateRow(lineIndex, line, maze));
 
-                // read next line
+                // Read next line
                 line = reader.readLine();
                 lineIndex++;
             }
+
+            // Validate number of lines read
             validateFileSize(lineIndex - 1);
         }
+        // Validate maze
+        validateMaze(maze);
+
+        // Set the matrix of the maze
         maze.setMatrix(convertListTo2DArray(statesList));
         return maze;
+    }
+
+    private static void validateMaze(Maze maze) throws InvalidInputException {
+        if (maze.getStartPoint() == null) {
+            throw new InvalidInputException("Maze does not contain a Start point");
+        }
+        if (maze.getGoalPoint() == null) {
+            throw new InvalidInputException("Maze does not contain a Goal point");
+        }
     }
 
     private static void validateFileSize(int length) throws InvalidInputException {
         if (length < 0) {
             throw new InvalidInputException("File size is more than lower-limit");
-        }
-        else if (length < 2) {
+        } else if (length < 2) {
             throw new InvalidInputException("File size is less than lower-limit");
         }
     }
@@ -59,12 +89,14 @@ public class InputRead {
         EPointState[] pointStates = new EPointState[line.length()];
         for (int i = 0; i < line.length(); i++) {
             char nextChar = line.charAt(i);
+
+            // Validate the last read char
             validateChar(nextChar, maze);
 
             EPointState pointState = EPointState.getPointState(nextChar);
-            //Store point-state in the array
+            // Store point-state in the array
             pointStates[i] = pointState;
-            //Set start/goal point in maze
+            // Set start/goal point in maze
             if (pointState == EPointState.START) {
                 maze.setStartPoint(new Point(rowIndex, i));
             } else if (pointState == EPointState.GOAL) {
