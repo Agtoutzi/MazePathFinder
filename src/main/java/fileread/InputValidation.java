a77919fc2d5e96b705973bade7e45da9a71807e5
Angelos <toutziaris.angelos@outlook.com>
2019-12-20 13:23:50 +0200

    Add check + add javadoc in input-validation class

--- src/main/java/fileread/InputValidation.java
+++ src/main/java/fileread/InputValidation.java
@@ -11,7 +11,17 @@ public class InputValidation {
     private InputValidation() {
     }
 
+    /**
+     * Validates a given {@link Maze}.
+     *
+     * @param maze the {@link Maze} to be validated
+     * @throws InvalidInputException if given maze is null, maze's START/GOAL point is null or equal to each-other, and
+     *                               the size of the maze is within limits
+     */
     public static void validateMaze(Maze maze) throws InvalidInputException {
+        if (maze == null) {
+            throw new InvalidInputException("Maze is null");
+        }
         if (maze.getStartPoint() == null) {
             throw new InvalidInputException("Maze does not contain a Start point");
         }
@@ -24,26 +34,51 @@ public class InputValidation {
         try {
             validateFileSize(maze.getRowLength());
             validateFileSize(maze.getColumnLength());
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             throw new InvalidInputException("Maze is invalid");
         }
     }
 
+    /**
+     * Validates the size of the read text file.
+     *
+     * @param length the length value to be validated
+     * @throws InvalidInputException if the given length value is not within limits
+     */
     static void validateFileSize(int length) throws InvalidInputException {
         if (length < 0) {
-            throw new InvalidInputException("File size is more than lower-limit");
+            throw new InvalidInputException("File size is more than upper-limit");
         } else if (length < 2) {
             throw new InvalidInputException("File size is less than lower-limit");
         }
     }
 
-    static void validateLineLength(String line, int firstLineLength) throws InvalidInputException {
-        if (line.length() != firstLineLength) {
+    /**
+     * Validates a given line for its length.
+     *
+     * @param line        the line to be validated
+     * @param validLength the length that is considered valid
+     * @throws InvalidInputException if the given line is null or line length is not within limits or line length does not
+     *                               match the given valid length
+     */
+    static void validateLineLength(String line, int validLength) throws InvalidInputException {
+        if (line == null) {
+            throw new InvalidInputException("Line is null");
+        }
+        validateFileSize(line.length());
+        if (line.length() != validLength) {
             throw new InvalidInputException("Rows do not have the same length");
         }
     }
 
+    /**
+     * Validates a given character. Checks whether the character is supported and if the character is START/GOAL
+     * character, checks the given {@link Maze} to see if the START/GOAL character is already defined.
+     *
+     * @param mazeCharacter the character to validate
+     * @param maze          the maze to check for existing START/GOAL point
+     * @throws InvalidInputException if the given character is invalid
+     */
     static void validateChar(char mazeCharacter, Maze maze) throws InvalidInputException {
         EPointState pointState = EPointState.getPointState(mazeCharacter);
         if (pointState == null) {
