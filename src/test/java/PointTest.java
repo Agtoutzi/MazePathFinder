d30d93e84ea241222858dd4be118ebe58a0c45dc
Angelos <toutziaris.angelos@outlook.com>
2019-12-20 09:36:31 +0200

    Add Point test class

---
+++ src/test/java/PointTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void getX() {
+    }
+
+    @Test
+    public void getY() {
+    }
+}
