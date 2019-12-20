356c4f14cf65ce2fa4e130f404458c642c056d9c
Angelos <toutziaris.angelos@outlook.com>
2019-12-20 09:43:36 +0200

    Implement test methods for Point class

--- src/test/java/PointTest.java
+++ src/test/java/PointTest.java
@@ -1,14 +1,28 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class PointTest {
 
     @Test
     public void getX() {
+        Point point = new Point(5, 5);
+        assertEquals(5, point.getX());
     }
 
     @Test
     public void getY() {
+        Point point = new Point(1, 100);
+        assertEquals(100, point.getY());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getX_Negative() {
+        new Point(-5, 5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getY_Negative() {
+        new Point(1, -100);
     }
 }
